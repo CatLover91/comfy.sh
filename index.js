289e80ce3a05723d191b41582ddc5fab9bbf5711
Nicholai <nicholai@zoomph.com>
2019-03-14 14:43:36 -0400

    added commander, seperated files

--- index.js
+++ index.js
@@ -1,63 +1,25 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const Handlebars = require('handlebars')
 const program = require('commander')
+const toolkit = require('./lib/toolkit')
 
-const defaultConfig = {
-    os: process.platform,
-    home: require('os').homedir(),
-    delim: process.platform === 'win32' ? '\\' : '/',
-    ignore: ['config.json', 'README.md', '.gitignore', '.git', 'node_modules']
-}
-function clean(path) {
-    fs.unlinkSync(path);
-}
-function copy(source, output) {
-    fs.copyFile(source, output, (err) => {
-        if (err) throw err;
-        console.log('copied file: ' + source)
+program
+    .version('0.0.1', '-v, --version')
+
+program
+    .command('copy [dir]')
+    .description('copies over files and overwrites old files')
+    .action((dir) => {
+        console.log('Copying...')
+        toolkit.loadDir(dir ? dir : '.', toolkit.copy)
     })
-}
-function handleFile(dir, name, config) {
-    console.log('LOAD FILE', dir, name, config)
 
-    let path;
-    if (config.output) {
-        console.log('Output override')
-        path = Handlebars.compile(config.output)(defaultConfig) + defaultConfig.delim + name
-    } else {
-        console.log('Output Home Default')
-        path = defaultConfig.home + defaultConfig.delim + dir + defaultConfig.delim + name
-    } 
-    console.log('path to operate from: ', __dirname + defaultConfig.delim + dir + defaultConfig.delim + name)
-    console.log('path to operate on: ', path)
-    
-}
+program
+    .command('clean [dir]')
+    .description('deletes destination files that are already covered by comfy')
+    .action((dir) => {
+        console.log('Cleaning...')
+        toolkit.loadDir(dir ? dir : '.', toolkit.clean)
+    })
 
-function loadDir(dir, config) { 
-
-    fs.readdir(dir, function(err, items) {
-        let curConfig = defaultConfig
-        for (let i=0; i<items.length; i++)
-            if(items[i] === 'config.json')
-                curConfig = JSON.parse(fs.readFileSync(dir + '/' + items[i], 'utf8'))
-        
-        
-        for (let i=0; i<items.length; i++) {
-            if(!curConfig.ignore || curConfig.ignore.filter((ign) => ign === items[i]).length === 0) {
-                if(defaultConfig.ignore.filter((ign) => ign === items[i]).length === 0) {
-                    let file = dir === '.' ? items[i] : dir + defaultConfig.delim + items[i];
-                    let stats = fs.lstatSync(file)
-                    //console.log(file, stats)
-                    if(stats.isDirectory())
-                        loadDir(file, config)
-                    else
-                        handleFile(dir, items[i], curConfig)
-                }
-            }
-        }
-    });
-} 
-
-loadDir(process.argv.length <= 2 ? '.' : process.argv[2], defaultConfig)
+program.parse(process.argv)
